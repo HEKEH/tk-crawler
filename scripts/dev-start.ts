c1030ad6fd61d0da39abe4594ad105f471f516c8
Will Ho <kai.h@decode-group.com>
2024-12-30 11:31:09 +0800

    feat: init server

--- scripts/dev-start.ts
+++ scripts/dev-start.ts
@@ -5,18 +5,11 @@ import { LogColor, startProjects } from './utils';
 /** Projects start order */
 const projects: Array<ProjectConfig | Array<ProjectConfig>> = [
   {
-    name: 'el-popover',
-    path: join(__dirname, '../packages/el-popover'),
+    name: 'server',
+    path: join(__dirname, '../apps/server'),
     command: 'pnpm dev',
-    readySignal: /^built in \d+ms\.$/,
+    readySignal: />>>\s+server listen on/,
     logColor: LogColor.seafoam,
-  },
-  {
-    name: '@el-popover/demo',
-    path: join(__dirname, '../packages/demo'),
-    command: 'pnpm dev',
-    readySignal: /Local:\s+http:\/\/localhost:\d+\//,
-    logColor: LogColor.mint,
   },
 ];
 

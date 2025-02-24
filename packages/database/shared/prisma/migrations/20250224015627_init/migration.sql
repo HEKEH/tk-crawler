6fa3cfee9248da8cb99e2fe2ee29c39168019313
Will Ho <kai.h@decode-group.com>
2025-02-24 12:19:30 +0800

    feat: mysql table schema

--- packages/database/shared/prisma/migrations/20250224015627_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `Anchor` (
-    `user_id` VARCHAR(191) NOT NULL,
-    `display_id` VARCHAR(191) NOT NULL,
-    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updated_at` DATETIME(3) NOT NULL,
-
-    PRIMARY KEY (`user_id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ packages/database/shared/prisma/migrations/20250224043006_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE `Anchor` (
+    `user_id` BIGINT NOT NULL,
+    `display_id` VARCHAR(30) NOT NULL,
+    `region` CHAR(2) NOT NULL,
+    `follower_count` INTEGER UNSIGNED NOT NULL,
+    `audience_count` INTEGER UNSIGNED NOT NULL,
+    `current_diamond` INTEGER UNSIGNED NOT NULL,
+    `last_diamond` INTEGER UNSIGNED NULL,
+    `highest_diamond` INTEGER UNSIGNED NULL,
+    `rank_league` CHAR(2) NOT NULL,
+    `level` TINYINT UNSIGNED NULL,
+    `has_commerce_goods` BOOLEAN NULL,
+    `tag` VARCHAR(20) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Anchor_display_id_key`(`display_id`),
+    INDEX `Anchor_region_idx`(`region`),
+    INDEX `Anchor_follower_count_idx`(`follower_count`),
+    INDEX `Anchor_highest_diamond_idx`(`highest_diamond`),
+    INDEX `Anchor_rank_league_idx`(`rank_league`),
+    INDEX `Anchor_level_idx`(`level`),
+    PRIMARY KEY (`user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

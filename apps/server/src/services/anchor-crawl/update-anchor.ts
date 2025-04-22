e193c289a408d971ca7dd99ac16f79d472932ef9
Will Ho <kai.h@decode-group.com>
2025-04-22 22:02:18 +0800

    feat: update diamond get

--- apps/server/src/services/anchor-crawl/update-anchor.ts
+++ apps/server/src/services/anchor-crawl/update-anchor.ts
@@ -55,16 +55,16 @@ export async function updateAnchor(data: UpdateAnchorRequest) {
   )
   ON DUPLICATE KEY UPDATE
     display_id = ${display_id},
+    -- 如果房间id发生变化，则更新last_diamonds
+    last_diamonds = IF(room_id != VALUES(room_id), current_diamonds, last_diamonds),
+    -- 因为current_diamonds的计算方式是有问题的，总是会小于或等于真实的钻石数，所以如果之前的current_diamonds较大，则不更新
+    current_diamonds = IF(room_id != VALUES(room_id), ${current_diamonds}, GREATEST(${current_diamonds}, current_diamonds)),
+    highest_diamonds = GREATEST(${current_diamonds}, highest_diamonds),
     room_id = ${room_id},
     region = ${region},
     follower_count = ${follower_count},
     audience_count = ${audience_count},
     level = ${level},
-    -- 因为current_diamonds的计算方式是有问题的，总是会小于或等于真实的钻石数，所以如果之前的current_diamonds较大，则不更新
-    current_diamonds = IF(room_id != ${room_id}, ${current_diamonds}, GREATEST(${current_diamonds}, current_diamonds)),
-    -- 如果房间id发生变化，则更新last_diamonds
-    last_diamonds = IF(room_id != ${room_id}, current_diamonds, last_diamonds),
-    highest_diamonds = GREATEST(${current_diamonds}, highest_diamonds),
     rank_league = ${rank_league},
     has_commerce_goods = ${has_commerce_goods},
     tag = ${tag},

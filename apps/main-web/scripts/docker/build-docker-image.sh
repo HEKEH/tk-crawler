670de10c636a18dab4d6acd1021e5293f24c69e2
Will Ho <kai.h@decode-group.com>
2025-06-01 16:39:54 +0800

    chore: main-web docker script

--- apps/main-web/scripts/docker/build-docker-image.sh
+++ apps/main-web/scripts/docker/build-docker-image.sh
@@ -108,6 +108,7 @@ function remove_image() {
 
 function build_image() {
   log "Building docker image ${IMAGE_TAG}"
+  export DOCKER_BUILDKIT=1
   local build_cmd="docker build . -f "${SCRIPT_DIR}/Dockerfile" --platform linux/amd64 -t "${IMAGE_TAG}""
 
   if [ "$NO_CACHE" = true ]; then
